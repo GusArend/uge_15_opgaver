bb8cadd85b5efe502758f3c24d32091518f172ae
Gustav Rasmussen <gustavarend@me.com>
2024-04-09 12:15:58 +0200

    get value og items

---
+++ uge_15_opgaver/Utility.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace uge_15_opgaver
+{
+    public class Utility
+    {
+        public double GetValueOfBook(Book book) {
+            return book.Price;
+        }
+
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            double price = 0;
+            switch (amulet.Quality)
+            {
+                case Level.high:
+                    price= 27.5;
+                    break;
+                case Level.medium:
+                    price= 20.0;
+                    break;
+                case Level.low:
+                    price= 12.5;
+                    break;
+
+            }
+                return price;
+            
+        }
+    }
+}
