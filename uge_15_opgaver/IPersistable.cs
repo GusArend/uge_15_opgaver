f27955a01b6eb86920edfafe4dfd390eff9333a6
Gustav Rasmussen <gustavarend@me.com>
2024-04-19 11:31:05 +0200

    ff

--- uge_15_opgaver/IPersistable.cs
+++ uge_15_opgaver/IPersistable.cs
@@ -5,9 +5,7 @@ namespace uge_15_opgaver
     public interface IPersistable
     {
         
-        void Save(string? filename = "ValuableRepository.txt");
-
-        
-        void Load(string? filename = "ValuableRepository.txt");
+        void Save(string? filename);
+        void Load(string? filename);
     }
 }
